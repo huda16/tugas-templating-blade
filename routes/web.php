a84d88c88bd4c896c20b0313e3f0b539d1954361
huda16 <mochamadnurulhuda16@gmail.com>
2020-07-02 17:19:51 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,19 @@ Route::get('/laravel', function () {
     return view('welcome');
 });
 
-Route::get('/', 'RegisterController@home');
+Route::get('/home', 'RegisterController@home');
 Route::get('/register', 'RegisterController@register');
 Route::get('/welcome', 'RegisterController@sapa');
 Route::post('/welcome', 'RegisterController@welcome_post');
+
+Route::get('/master', function () {
+    return view('adminlte/master');
+});
+
+Route::get('/', function () {
+    return view('items/index');
+});
+
+Route::get('/data-tables', function () {
+    return view('items/indextabel');
+});
